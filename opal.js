43a69f7453b7386965923c92f459f08157e19d89
Dorumin <lautarobensoni@hotmail.com>
2017-08-19 20:29:53 -0300

    updating

--- opal.js
+++ opal.js
@@ -157,7 +157,7 @@ OpalBot.commands.admin.eval = (message, content) => {
     try {
         var r = eval(content);
         if (r) {
-            message.reply(r.toString());
+            message.reply(String(r));
         }
     } catch(e) {
         message.reply('ERROR: ' + e);

5a506296c46f7437723c7ca6b9eeba4f119dfefe
Dorumin <lautarobensoni@hotmail.com>
2017-08-20 19:09:04 -0300

    updating

--- opal.js
+++ opal.js
@@ -129,7 +129,8 @@ OpalBot.commands.peasants.ping = (message, content) => {
             message.channel.send(i18n.msg('cannot-edit', 'ping'));
             return;
         }
-        msg.edit(msg.content + '\n' + i18n.msg('result', 'ping', msg.createdTimestamp - message.createdTimestamp));
+        var latency = msg.createdTimestamp - message.createdTimestamp;
+        msg.edit(msg.content + '\n' + i18n.msg('result', 'ping', latency) + (latency > 300 ? '\n' + i18n.msg('diss', 'ping') : ''));
     });
 };
 

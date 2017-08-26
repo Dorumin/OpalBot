a90e779867acfcbefe13d629cbbd267856deaed0
Dorumin <lautarobensoni@hotmail.com>
2017-08-26 18:55:03 -0300

    scoping

--- i18n/index.js
+++ i18n/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 
-fs.readdirSync('../i18n')
+fs.readdirSync('./i18n')
     .filter(file => file.endsWith('.json'))
     .forEach(title => exports[title.slice(0, -5)] = require('./' + title));

3894de533a2304a6a4285194605da5adf64a48cc
Dorumin <lautarobensoni@hotmail.com>
2017-08-27 20:11:18 -0300

    updating

--- commands/akinator.js
+++ commands/akinator.js
@@ -129,7 +129,7 @@ module.exports.peasants.akinator = async function(message, content, lang, i18n, 
     },
     akinator = new Akinator(),
     q = await akinator.init(lang, message.author.id);
-    message.channel.send('This is a test question.');
+    message.channel.send(JSON.stringify(q, null, 2));
     var res = await ask({triggers: ['r1', 'r2'], channel: message.channel.id, user: message.author.id});
     message.channel.send('Caught response');
     console.log(res.index);

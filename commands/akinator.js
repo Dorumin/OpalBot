dd90e7209c56013d7864d9b1ee7ff7fc313e705d
Dorumin <lautarobensoni@hotmail.com>
2017-08-27 22:32:30 -0300

    that too

--- commands/akinator.js
+++ commands/akinator.js
@@ -1,9 +1,4 @@
 const request = require('request');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
 class Akinator {
     get(url) {

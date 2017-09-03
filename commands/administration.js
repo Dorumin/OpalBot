2401d68024152aa7dc18c8256acfad7379c46584
Dorumin <lautarobensoni@hotmail.com>
2017-09-02 23:13:04 -0300

    kicking all users without any roles with !prune 0

--- commands/administration.js
+++ commands/administration.js
@@ -7,8 +7,47 @@ module.exports.admin.prune = async (message, content, lang, i18n, OpalBot) => {
     } else if (isNaN(count = parseInt(content, 10))) {
         message.reply(i18n.msg('invalid', 'prune', lang));
         return;
-    } else if (count == 0) {
-        message.reply(i18n.msg('non-zero', 'prune', lang));
+    } else if (count == 0) { // Kicking all users without any roles
+        var members = (await message.guild.fetchMembers()).members.filter(user => !(user.roles.size - 1)),
+        reason = content.slice(1).trim();
+        if (!members.size) {
+            message.reply(i18n.msg('no-roleless', 'prune', lang));
+            return;
+        }
+        var blocked = OpalBot.unprefixed.push({
+            type: 'prune',
+            triggers: [
+                i18n.msg('confirm', 'main', lang),
+                i18n.msg('cancel', 'main', lang)
+            ],
+            user: message.author.id,
+            channel: message.channel.id,
+            caseinsensitive: true,
+            timeout: 30000,
+            callback: async (message, index) => {
+                if (index == 0) { // confirm
+                    try {
+                        message.channel.send(i18n.msg('pruning', 'prune', lang));
+                        for (var [key, user] of members) {
+                            await user.kick(reason);
+                        }
+                        message.channel.send(i18n.msg('pruned', 'prune', members.size, lang));
+                    } catch(e) {
+                        message.channel.send(i18n.msg('missing-permissions', 'prune', lang));
+                    }
+                } else { // cancel
+                    message.channel.send(i18n.msg('cancelled', 'prune', lang));
+                }
+            },
+            ontimeout: () => {
+                message.channel.send(i18n.msg('timed-out', 'prune', lang));
+            }
+        });
+        if (blocked === true) {
+            message.channel.send(i18n.msg('blocked', 'prune', lang));
+        } else {
+            message.channel.send(i18n.msg('roleless-prompt', 'prune', members.size, lang));
+        }
         return;
     }
     try {

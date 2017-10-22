9b8d41921f9840849dd5d0be8b4691b4641f1769
Dorumin <lautarobensoni@hotmail.com>
2017-10-22 10:51:33 -0300

    decreasing max size so robyn stops crying like a baby

--- commands/games.js
+++ commands/games.js
@@ -1172,7 +1172,7 @@ module.exports.peasants.typingcontest = async (message, content, lang, i18n, Opa
         }),
         i = quotes.length;
         while (i--) {
-            if (quotes[i].content.length > 150  && quotes[i].content.length < 800 && quotes[i].content.indexOf('\n') == -1) {
+            if (quotes[i].content.length > 150  && quotes[i].content.length < 400 && quotes[i].content.indexOf('\n') == -1) {
                 quote = quotes[i];
                 break;
             }

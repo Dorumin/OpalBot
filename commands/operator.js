670c9f32f394b121c1f9d6ed302d098eaa34269b
Dorumin <lautarobensoni@hotmail.com>
2017-12-03 15:48:01 -0300

    +setgame

--- commands/operator.js
+++ commands/operator.js
@@ -14,7 +14,7 @@ module.exports.operator.eval = (message, content, lang, i18n, OpalBot) => {
             }
         })();`);
     } catch(e) {
-        message.channel.send(e);
+        message.channel.send(e).catch(OpalBot.util.log);
     }
 };
 
@@ -28,9 +28,9 @@ module.exports.operator.say = (message, content, lang, i18n, OpalBot) => {
     try {
         var r = eval(content);
         if (r == null || !r.toString().trim()) throw r;
-        message.channel.send(r.toString().trim());
+        message.channel.send(r.toString().trim()).catch(OpalBot.util.log);
     } catch(e) {
-        message.channel.send(content);
+        message.channel.send(content).catch(OpalBot.util.log);
     }
 };
 
@@ -43,23 +43,34 @@ module.exports.operator.gist = (message, content, lang, i18n, OpalBot) => {
     }
     request(`https://gist.github.com/${split.join('/')}/raw`, (err, r, body) => {
         if (err) return;
-        message.channel.send('Package loaded');
+        message.channel.send('Package loaded').catch(OpalBot.util.log);
         try {
             eval(body);
         } catch(e) {
-            message.channel.send(e.toString());
+            message.channel.send(e.toString()).catch(OpalBot.util.log);
         }
     });
 };
 
-module.exports.operator.script = (message, content) => {
+module.exports.operator.script = (message, content, lang, i18n, OpalBot) => {
     request(content, (err, r, body) => {
         if (err) return;
-        message.channel.send('Package loaded');
+        message.channel.send('Package loaded').catch(OpalBot.util.log);
         try {
             eval(body);
         } catch(e) {
-            message.channel.send(e.toString());
+            message.channel.send(e.toString()).catch(OpalBot.util.log);
         }
     });
+};
+
+module.exports.operator.game = 'setgame';
+module.exports.operator.setgame = (message, content, lang, i18n, OpalBot) => {
+    var match = content.match(/(?:https?:\/\/)?twitch.tv\/(\w+)/),
+    twitch = null;
+    if (match) {
+        content = content.slice(0, match.index) + content.slice(match.index + match[0].length);
+        twitch = 'http://twitch.tv/' + match[1];
+    }
+    OpalBot.client.user.setGame(content, twitch);
 };

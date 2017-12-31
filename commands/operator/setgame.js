47d1a198f206db65f3259155b57c57edcc1dd836
Enzo <dorminsama@gmail.com>
2017-12-31 16:20:32 -0300

    support setting streaming without url, watching and listening to statuses from !setgame; adding !setpresence alias

--- commands/operator/setgame.js
+++ commands/operator/setgame.js
@@ -4,14 +4,30 @@ module.exports = (OpalBot) => {
     
     out.operator = {};
     out.operator.game = 'setgame';
+    out.operator.setpresence = 'setgame';
     out.operator.setgame = (message, content, lang) => {
         let match = content.match(/(?:https?:\/\/)?(?:[\w-]+\.)?twitch.tv\/(\w+)/),
-        twitch = null;
+        twitch = null,
+        types = i18n.msg('types', 'setgame', lang).split('|'),
+        type = 0;
         if (match) {
             content = content.slice(0, match.index) + content.slice(match.index + match[0].length);
             twitch = 'http://twitch.tv/' + match[1];
         }
-        OpalBot.client.user.setGame(content, twitch);
+        for (let i in types) {
+            if (content.startsWith(types[i])) {
+                type = i;
+                content = content.slice(types[i].length).trim();
+                break;
+            }
+        }
+        OpalBot.client.user.setPresence({
+            game: {
+                name: content,
+                url: twitch,
+                type: type
+            }
+        })
     };
 
     return out;

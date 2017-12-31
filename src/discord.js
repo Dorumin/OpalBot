90cd59d6b8f46b908e39c9e6e7a5d703b68d6fca
Enzo <dorminsama@gmail.com>
2017-12-31 16:00:56 -0300

    new statuses polyfill for discord.js

--- src/discord.js
+++ src/discord.js
@@ -7,5 +7,54 @@ module.exports = (OpalBot) => {
 
     require('./listeners.js')(OpalBot);
 
+    OpalBot.client.user.setPresence = (data) => {
+        return new Promise(resolve => {
+          let status = this.localPresence.status || this.presence.status;
+          let game = this.localPresence.game;
+          let afk = this.localPresence.afk || this.presence.afk;
+    
+          if (!game && this.presence.game) {
+            game = {
+              name: this.presence.game.name,
+              type: this.presence.game.type,
+              url: this.presence.game.url,
+            };
+          }
+    
+          if (data.status) {
+            if (typeof data.status !== 'string') throw new TypeError('Status must be a string');
+            if (this.bot) {
+              status = data.status;
+            } else {
+              this.settings.update(Constants.UserSettingsMap.status, data.status);
+              status = 'invisible';
+            }
+          }
+    
+          if (data.game) {
+            game = data.game;
+            game.type = game.type === undefined ? (game.url ? 1 : 0) : game.type;
+          } else if (typeof data.game !== 'undefined') {
+            game = null;
+          }
+    
+          if (typeof data.afk !== 'undefined') afk = data.afk;
+          afk = Boolean(afk);
+    
+          this.localPresence = { status, game, afk };
+          this.localPresence.since = 0;
+          this.localPresence.game = this.localPresence.game || null;
+    
+          this.client.ws.send({
+            op: 3,
+            d: this.localPresence,
+          });
+    
+          this.client._setPresence(this.id, this.localPresence);
+    
+          resolve(this);
+        });
+    };
+
     client.login(config.token).catch(OpalBot.util.log);
 };

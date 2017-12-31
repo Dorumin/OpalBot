598f57ef4e5c4ae4247c081abe2a6edc73b60984
Enzo <dorminsama@gmail.com>
2017-12-31 00:10:45 -0300

    x4

--- commands/utility/imagesearch.js
+++ commands/utility/imagesearch.js
@@ -14,6 +14,7 @@ module.exports = (OpalBot) => {
             embed: {
                 color: OpalBot.color,
                 title: i18n.msg('title', 'imagesearch', lang),
+                url: 'https://www.google.com/searchbyimage?image_url=' + encodeURIComponent(url),
                 image: {
                     url: url
                 },

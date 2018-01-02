81f296843690676f1c9971f8c02edd67a8251f95
Enzo <dorminsama@gmail.com>
2018-01-02 20:24:56 -0300

    set proper content-type headers

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const http = require('http'),
+path = require('path'),
 fs = require('fs'),
 request = require('request'),
 config = require('./config.js');
@@ -6,6 +7,13 @@ config = require('./config.js');
 module.exports = (OpalBot) => {
 
     OpalBot.paths = require('./paths.js')(OpalBot);
+    OpalBot.contentTypes = {
+        '.html': 'text/html',
+        '.css':  'text/css',
+        '.js':   'text/javascript',
+        '.json': 'text/json',
+        '.svg':  'image/svg+xml'
+    };
     OpalBot.server = http.createServer((req, res) => {
         let path = req.url.slice(1).split('?')[0].split('/')[0];
         if (OpalBot.paths[path]) {
@@ -23,6 +31,9 @@ module.exports = (OpalBot) => {
             OpalBot.paths['404'](req, res);
         });
         stream.on('open', () => {
+            res.writeHead(200, {
+                'Content-Type': OpalBot.contentTypes[path.extname(req.url)]
+            });
             stream.pipe(res);
         });
     }).listen(config.PORT || 5000);

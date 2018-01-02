0fb5a05d17dd882d3b17a932495fdb74cb239d18
Enzo <dorminsama@gmail.com>
2018-01-02 19:47:22 -0300

    oops

--- www/js/util.js
+++ www/js/util.js
@@ -3,5 +3,5 @@ window.util = {};
 util.format_usage = function(str) {
     return str
         .replace(/\(required\)/g, '<b>(required)</b>')
-        .replace(/\[(.+)\]/g, '<span class="optional">$1</span>');
+        .replace(/\[.+\]/g, '<span class="optional">$&</span>');
 };

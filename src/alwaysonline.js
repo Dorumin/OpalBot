c9897eb75af242bc67819759534b2340dfd62e0a
Enzo <dorminsama@gmail.com>
2018-01-06 22:41:22 -0300

    readd

---
+++ src/alwaysonline.js
@@ -0,0 +1,106 @@
+// This file isn't supposed to be pretty, or well coded, or maintainable. It's a hack.
+
+const request = require('request'),
+config = require('./config.js'),
+
+get_ms_until_next_swap = (d = new Date()) => {
+    if (config.is_backup) {
+        return (
+            d.getDate() >= 15 ?
+            new Date(d.getFullYear(), d.getMonth() + 1, 1).getTime() :
+            0
+        ) - d.getTime()
+    } else {
+        return (
+            d.getDate() < 15 ?
+            new Date(d.getFullYear(), d.getMonth(), 15).getTime() :
+            0
+        ) - d.getTime()
+    }
+},
+
+get_ids = (appname, token) => {
+    return new Promise((res, rej) => {
+        request('https://api.heroku.com/teams/apps/' + appname, {
+            headers: {
+                Authorization: 'Bearer ' + token,
+                Accept: 'application/vnd.heroku+json; version=3'
+            }
+        }, (err, r, body) => {
+            if (err) {
+                rej(err);
+                return;
+            }
+
+            body = JSON.parse(body);
+
+            request(`https://api.heroku.com/apps/${body.id}/formation`, {
+                headers: {
+                    Authorization: 'Bearer ' + token,
+                    Accept: 'application/vnd.heroku+json; version=3'
+                }
+            }, (err, r, body) => {
+                if (err) {
+                    rej(err);
+                    return;
+                }
+
+                body = JSON.parse(body)[0];
+
+                res([body.app.id, body.id]);
+            });
+        });
+    });
+},
+
+get_all_ids = (config) => {
+    return Promise.all([
+        get_ids(config.app_name, config.heroku_token),
+        get_ids(config.backup_app_name, config.backup_heroku_token)
+    ]);
+},
+
+scale = (ids, num, token) => {
+    return new Promise((res, rej) => {
+        request.patch(`https://api.heroku.com/apps/${ids[0]}/formation/${ids[1]}`, {
+            form: {
+                quantity: num,
+                size: 'Free',
+                type: 'web'
+            },
+            headers: {
+                Authorization: 'Bearer ' + token,
+                Accept: 'application/vnd.heroku+json; version=3'
+            }
+        }, (err, r, body) => {
+            if (err) {
+                rej(err);
+                return;
+            }
+
+            res(body);
+        });
+    });
+};
+
+get_all_ids(config).then(arr => {
+    let [
+        app,
+        backup
+    ] = arr,
+    d = new Date();
+
+    setTimeout(() => {
+        scale( // turn on that other app
+            config.is_backup ? app : backup,
+            1,
+            config.is_backup ? config.heroku_token : config.backup_heroku_token
+        ).then(() => {
+            scale( // turn off our app
+                config.is_backup ? backup : app,
+                0,
+                config.is_backup ? config.backup_heroku_token : config.heroku_token
+            )
+        })
+    }, Math.max(get_ms_until_next_swap(), 0));
+});

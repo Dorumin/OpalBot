9ce6ca7ce96ac01968b1c368b78f261becd3264b
Enzo <dorminsama@gmail.com>
2018-01-30 01:08:33 -0300

    message.reply > message.channel.send

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -8,7 +8,7 @@ module.exports = (OpalBot) => {
         let ping = message.content.indexOf('ping') + 1 || 1000,
         pong = message.content.indexOf('pong') + 1 || 1001,
         d1 = Date.now(); // Don't tell me to use message.createdTimestamp. That can return negative values.
-        message.reply(ping < pong ? i18n.msg('pong', 'ping', lang) : i18n.msg('ping', 'ping', lang)).then(msg => {
+        message.channel.send(ping < pong ? i18n.msg('pong', 'ping', lang) : i18n.msg('ping', 'ping', lang)).then(msg => {
             let latency = Date.now() - d1;
             if (!msg.editable) {
                 message.channel.send(i18n.msg('result', 'ping', latency, lang)).catch(OpalBot.util.log);

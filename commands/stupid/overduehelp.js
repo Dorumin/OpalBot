1492102c6cb8e6b941ee4e6059113be0e406c96f
Robyn <32017811+GameModerator@users.noreply.github.com>
2019-04-17 18:52:22 +0800

    move along nothing to see here just a typo

--- commands/stupid/overduehelp.js
+++ commands/stupid/overduehelp.js
@@ -2,7 +2,7 @@ module.exports = (OpalBot) => {
     const out = {}; 
     
     out.peasants = {};
-    out.peasants.help = (message) => message.replay('https://opalbot.herokuapp.com/commands')
+    out.peasants.help = (message) => message.reply('https://opalbot.herokuapp.com/commands')
 
     return out;
 };

298ded853e3411b26cc4eecca8b1c6ca246f6703
Dorumin <dorminsama@gmail.com>
2019-04-22 22:56:51 -0300

    same

--- commands/stupid/swag.js
+++ commands/stupid/swag.js
@@ -2,7 +2,7 @@ module.exports = (OpalBot) => {
     const out = {}; 
     
     out.peasants = {};
-    out.peasants.me = 'swag';
+    // out.peasants.me = 'swag';
     out.peasants.swagger = 'swag';
     out.peasants.swag = (message, content, lang) => {
         message.channel.send(':point_up_2: The user above me has some serious swag.').catch(OpalBot.util.log);

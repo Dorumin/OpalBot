a9217bccd0b4763d123f04ac4100dc9b5b267936
Dorumin <dorminsama@gmail.com>
2019-04-25 12:02:55 -0300

    it never works the first time around... why

--- commands/utility/lyrics.js
+++ commands/utility/lyrics.js
@@ -31,7 +31,7 @@ module.exports = (OpalBot) => {
     out.peasants.lyrics = async (message, content) => {
         const res = await got(`https://lyricsfa.com/?s=${encodeURIComponent(content)}&orderby=relevance&order=DESC`);
         const $ = cheerio.load(res.body);
-        const link = $('.entry-title a.dark').first().find('a').attr('href');
+        const link = $('.entry-title a.dark').first().attr('href');
         const lyricsRes = await got(link);
         const $l = cheerio.load(lyricsRes.body);
         const lyrics = $l('.entry.entry-single').text()

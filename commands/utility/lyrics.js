694cd46f7a456e1958c772a0cbc6c2f0e451e77b
Dorumin <dorminsama@gmail.com>
2019-04-25 18:06:24 -0300

    Absolute genius, and i18n

--- commands/utility/lyrics.js
+++ commands/utility/lyrics.js
@@ -24,24 +24,64 @@ function chunk(arr, overhead = 0, jump = 1, max = 2000) {
     return chunks;
 }
 
+function formatLyrics(lyrics) {
+    const split = lyrics.split('\n').map(line => line.trim());
+
+    console.log(lyrics, split);
+
+    while (split[0].startsWith('[') || split[0] == '') {
+        split.splice(0, 1);
+    }
+
+    return split.join('\n').replace(/\n{2,}/g, '\n\n').replace(/\*/g, '\\*').trim();
+}
+
 module.exports = (OpalBot) => {
     const out = {};
 
     out.peasants = {};
-    out.peasants.lyrics = async (message, content) => {
-        const res = await got(`https://search.azlyrics.com/search.php?q=${encodeURIComponent(content)}`);
-        const $ = cheerio.load(res.body);
-        const link = $('.text-left.visitedlyr').first().find('a').attr('href');
-        const lyricsRes = await got(link);
-        const $l = cheerio.load(lyricsRes.body);
-        const title = $l('.ringtone + b').text();
-        const lyrics = $l('.ringtone + b + br + br + div').text().replace(/\n{2,}/g, '\n\n');
+    out.peasants.lyrics = async (message, content, lang) => {
+        const res = await got(`https://genius.com/api/search/song?page=1&q=${encodeURIComponent(content)}`, {
+            json: true
+        });
+        const songs = res.body.response.sections[0].hits
+            .sort((a, b) => {
+                var same1 = a.result.title.toLowerCase() == content.toLowerCase(),
+                same2 = b.result.title.toLowerCase() == content.toLowerCase();
+
+                if (same1 == same2) return b.result.stats.pageviews - a.result.stats.pageviews;
+
+                if (same1) return -1;
+                if (same2) return 1;
+            });
+        
+        console.log(songs);
+
+        const song = songs[0];
+        const lyricsRes = await got(`https://genius.com${song.result.path}`);
+        const $ = cheerio.load(lyricsRes.body);
+        const title = song.result.title;
+        const artist = song.result.primary_artist.name;
+        console.log($('.lyrics p').first().text());
+        const lyrics = formatLyrics($('.lyrics p').first().text());
         const split = chunk(lyrics.split('\n'), 10, 1, 2000);
 
-        await message.channel.send(`**${title}**`);
-
         for (let i = 0; i < split.length; i++) {
-            await message.channel.send(split[i]);
+            await message.channel.send({
+                embed: {
+                    color: OpalBot.color,
+                    title: i == 0
+                        ? OpalBot.i18n.msg('for', 'lyrics', title, artist, lang)
+                        : undefined,
+                    description: split[i].join('\n'),
+                    footer: i == split.length - 1
+                        ? {
+                            text: OpalBot.i18n.msg('footer', 'lyrics', message.member.nickname || message.author.username, lang),
+                            icon_url: message.author.displayAvatarURL
+                        }
+                        : undefined
+                }
+            });
         }
     };
 

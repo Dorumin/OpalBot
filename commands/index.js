494f44c21531b517cc09f7ec60ba36b0d89969b6
Dorumin <dorminsama@gmail.com>
2019-04-26 17:34:54 -0300

    better errors when I forget to export like a retard

--- commands/index.js
+++ commands/index.js
@@ -11,6 +11,10 @@ module.exports = (OpalBot) => {
             fs.readdirSync(path.join(c, file))
                 .forEach(command => {
                     let xport = require(path.join('.' + c, file, command))(OpalBot);
+                    if (!xport) {
+                        console.log(`Module not exported anything: ${file}`);
+                        return;
+                    }
                     for (let key in xport) {
                         out[key] = out[key] || {};
                         out[key] = {

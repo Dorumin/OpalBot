0665afe370e03337b3ef02bffbecd20023fb9c69
Robyn <32017811+GameModerator@users.noreply.github.com>
2019-05-06 16:59:52 +0800

    SYNTAX

--- commands/utility/rate.js
+++ commands/utility/rate.js
@@ -10,7 +10,8 @@ module.exports = (OpalBot) => {
         if (item === 'Doru' || 'Robyn') {
             message.channel.send(i18n.msg('rigsult', 'rate', `<@${message.author.id}>`, item, lang)).catch(OpalBot.util.log);
         } else {
-        message.channel.send(i18n.msg('result', 'rate', `<@${message.author.id}>`, item, result, lang)).catch(OpalBot.util.log);
+            message.channel.send(i18n.msg('result', 'rate', `<@${message.author.id}>`, item, result, lang)).catch(OpalBot.util.log);
+        }
     } 
     return out;
 };

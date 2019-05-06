6eabdbd81883ded51910f35357ace1749a3e0ba9
Robyn <32017811+GameModerator@users.noreply.github.com>
2019-05-06 16:37:23 +0800

    question mark

--- commands/utility/rate.js
+++ commands/utility/rate.js
@@ -6,7 +6,7 @@ module.exports = (OpalBot) => {
     out.peasants.r = 'rate';
     out.peasants.rate = (message, content, lang) => {
         result = Math.ceil(Math.random() * 10);
-        message.channel.send(i18n.msg('result', 'rate', `<@${message.author.id}>`, content, result, lang)).catch(OpalBot.util.log);
+        message.channel.send(i18n.msg('result', 'rate', `<@${message.author.id}>`, (content), result, lang)).catch(OpalBot.util.log);
     } 
     return out;
 };

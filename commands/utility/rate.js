ce5e8eeaa7e77f442993a06844cd4b4608b85db0
Robyn <32017811+GameModerator@users.noreply.github.com>
2019-05-06 19:09:20 +0800

    last edit for the next half an hour peaceful hour

--- commands/utility/rate.js
+++ commands/utility/rate.js
@@ -8,7 +8,7 @@ module.exports = (OpalBot) => {
         result = Math.ceil(Math.random() * 10);
         if (!content.trim()) {
             message.channel.send(i18n.msg('result', 'rate', `<@${message.author.id}>`, message.author.username, result, lang)).catch(OpalBot.util.log);
-        } else if (content == 'Doru') {
+        } else if (content == 'Doru' || 'Robyn' || message.mentions.users.has('187524257280950272') || message.mentions.users.has('155545848812535808')) {
             message.channel.send(i18n.msg('rigsult', 'rate', `<@${message.author.id}>`, content, lang)).catch(OpalBot.util.log);
         } else {
             message.channel.send(i18n.msg('result', 'rate', `<@${message.author.id}>`, content, result, lang)).catch(OpalBot.util.log);

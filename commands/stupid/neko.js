5ac3f11cdff0969a9ce8e19bcb59b54d591bd085
Enzo <dorminsama@gmail.com>
2019-06-12 19:01:05 -0300

    tfw

--- commands/stupid/neko.js
+++ commands/stupid/neko.js
@@ -1,12 +1,12 @@
 const got = require('got');
-module.exports = async (OpalBot) => {
+module.exports = (OpalBot) => {
     const out = {}; 
     
     out.peasants = {};
     out.peasants.nekos = 'neko';
     out.peasants.catgirl = 'neko';
     out.peasants.catgirls = 'neko';
-    out.peasants.neko = (message, content, lang) => {
+    out.peasants.neko = async (message, content, lang) => {
         try {
             const { body } = await got('https://nekos.life/api/v2/img/neko', { json: true });
             message.channel.send(body.url)

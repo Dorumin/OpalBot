eb422741d28d1ee43c03451a7b66b7bc027845c7
Dorumin <dorminsama@gmail.com>
2019-09-04 02:42:40 -0300

    deep json stringify response

--- commands/utility/translate.js
+++ commands/utility/translate.js
@@ -37,7 +37,7 @@ module.exports = (OpalBot) => {
             json: true
         });
 
-        OpalBot.util.log(body);
+        OpalBot.util.log(JSON.stingify(body, null, 2));
 
         message.channel.send(`> ${text}\n${body[0][0][0]}`);
     };

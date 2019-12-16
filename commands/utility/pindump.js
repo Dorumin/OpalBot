bc71e408078ef7665a997196adf072205e8626a8
Dorumin <dorminsama@gmail.com>
2019-12-16 12:03:16 -0300

    Update pindump

--- commands/utility/pindump.js
+++ commands/utility/pindump.js
@@ -20,15 +20,16 @@ const escape = str => str.replace(/`/g, '\\`');
 
 const pad = n => ('0' + n).slice(-2);
 
-const date = d => `${pad(d.getUTCDate())}/${pad(d.getUTCMonth() + 1)}/${pad(d.getUTCFullYear())} ${pad(d.getUTCHours())}:${pad(d.getUTCMinutes())}`;
+const date = d => `${pad(d.getUTCDate())}/${pad(d.getUTCMonth() + 1)}/${pad(d.getUTCFullYear())} ${pad(d.getUTCHours())}:${pad(d.getUTCMinutes())}:${pad(d.getUTCSeconds())}`;
 
 module.exports = (OpalBot) => {
     const out = {};
-    
+
     out.messages = {};
     out.messages.pinpilation = 'pindump';
     out.messages.pindump = async (message, content, lang) => {
-        const pins = await message.channel.fetchPinnedMessages();
+        const pins = await message.channel.fetchPinnedMessages(),
+        channel = message.mentions.channels.first() || message.channel;
 
         if (!pins.size) {
             message.channel.send(OpalBot.i18n.msg('no-pins', 'pindump', lang));
@@ -36,21 +37,31 @@ module.exports = (OpalBot) => {
         }
 
         const pieces = pins.map(message => {
-            const content = message.content.trim()
-                ? '```' + escape(message.author.username) + ' - ' + date(message.createdAt) + '\n' + escape(message.content) + '```'
-                :
-                message.embeds.length
-                    ? '```<embed>```'
-                    : '\n';
-            return content
-                + message.attachments.map(attachment => attachment.url + '\n').join('')
-                + `<https://discordapp.com/channels/${message.guild.id}/${message.channel.id}/${message.id}>`;
+            let content = '';
+
+            content += `\n> ${escape(message.author.username)} - ${date(message.createdAt)}`;
+
+            if (message.content) {
+                content += message.content.split('\n').map(line => `\n> ${line}`).join('');
+            }
+
+            if (message.embeds.length) {
+                content += '\n> <embed>';
+            }
+
+            if (message.attachments.size) {
+                content += message.attachments.map(a => `\n> ${a.url}`).join('');
+            }
+
+            content += `\n<https://discordapp.com/channels/${message.guild.id}/${message.channel.id}/${message.id}>`;
+
+            return content;
         });
         const chunked = chunk(pieces.reverse(), 2000);
         const links = [];
 
         for (let i = 0; i < chunked.length; i++) {
-            const compilation = await message.channel.send(chunked[i]);
+            const compilation = await channel.send(chunked[i]);
             links.push(`<https://discordapp.com/channels/${compilation.guild.id}/${compilation.channel.id}/${compilation.id}>`);
         }
 

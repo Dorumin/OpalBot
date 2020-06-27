c74ae2ac51bbcf461b7c0f02524512c147d7d49c
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2020-06-26 19:42:44 -0800

    WIP: add 404 handling

--- commands/stupid/http.js
+++ commands/stupid/http.js
@@ -1,11 +1,22 @@
+const got = require('got');
+
 module.exports = (OpalBot) => {
     const out = {}; 
     
     out.peasants = {};
-    out.peasants.http = (message, content, lang) => {
+    out.peasants.http = async (message, content, lang) => {
         if (!parseInt(content)) {
             return message.react('⚠');
         }
+        
+        const status = await got(`https://http.cat/${content}.jpg`, {
+            method: 'HEAD',
+            throwHttpErrors: false
+        });
+        if (status.statusCode === 404) {
+            return message.react('⚠');
+        }
+
         message.channel.send(`https://http.cat/${content}.jpg`);
     };
 
